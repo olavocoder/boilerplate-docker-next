e077a890667ab63d86702af9463db0d42c52a877
ferreiraViniciuss <vinicius.ferreira@raccoon.ag>
2022-11-03 15:33:14 -0300

    updating frontend configs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,24 +1,38 @@
-const withFonts = require('next-fonts')
-const withPWA = require('next-pwa')
+// const withFonts = require('next-fonts')
+// const withPWA = require('next-pwa')
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA(
-  withFonts({
-    pwa: {
-      dest: 'public',
-      disable: !isProd
-    },
-    experimental: {
-      amp: {
-        skipValidation: true
-      }
-    },
-    images: {
-      domains: [
-        process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
-        process.env.NEXT_PUBLIC_WORDPRESS_IMAGES
-      ]
-    }
-  })
-)
+// module.exports = withPWA(
+//   withFonts({
+//     pwa: {
+//       dest: 'public',
+//       disable: !isProd
+//     },
+//     experimental: {
+//       amp: {
+//         skipValidation: true
+//       }
+//     },
+//     images: {
+//       domains: [
+//         process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
+//         process.env.NEXT_PUBLIC_WORDPRESS_IMAGES
+//       ]
+//     }
+//   })
+// )
+
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/]
+})
+
+const nextConfig = withPWA({
+  // next config
+})
+module.exports = nextConfig
--- frontend/src/hooks/useSeoTreated.js
+++ frontend/src/hooks/useSeoTreated.js
@@ -1,13 +1,13 @@
 //Tratar as informações conforme a biblioteca:
 //https://www.npmjs.com/package/next-seo
 
-const UseSeoTreated = ({ seo, page }) => {
+const UseSeoTreated = ({ seo }) => {
   const title = seo.title
   const description = seo.description
   const canonical = seo.og_url
   const noindex = seo.robots.follow.index === 'noindex' ? true : false
 
-  data = {
+  const data = {
     title,
     description,
     canonical,
